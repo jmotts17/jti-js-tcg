024e33dd8717749d81033dcc15afa9037dec8ce6
Josh Motta <joshua.a.motta@gmail.com>
2021-05-20 16:19:19 -0400

    Completed truthy/falsy section

--- section2/basics-01-starting-project/assets/scripts/app.js
+++ section2/basics-01-starting-project/assets/scripts/app.js
@@ -30,11 +30,14 @@ function writeToLog(
 }
 
 function calculateResult(calculationType) {
-  if (
+    const enteredNumber = getUserNumberInput();
+    
+    if (
     calculationType !== 'ADD' &&
     calculationType !== 'SUBTRACT' &&
     calculationType !== 'MULTIPLY' &&
-    calculationType !== 'DIVIDE'
+    calculationType !== 'DIVIDE' ||
+    !enteredNumber
   ) {
     return;
   }
@@ -45,7 +48,7 @@ function calculateResult(calculationType) {
   //     calculationType === 'MULTIPLY' ||
   //     calculationType === 'DIVIDE'
   //   ) {
-  const enteredNumber = getUserNumberInput();
+
   const initialResult = currentResult;
   let mathOperator;
   if (calculationType === 'ADD') {
