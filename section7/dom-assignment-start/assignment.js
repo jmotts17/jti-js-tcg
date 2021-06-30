33b8dc93d25b79e8d5540567d750876983abe9a0
Josh Motta <joshua.a.motta@gmail.com>
2021-06-30 09:04:34 -0400

    Continued Section 7

---
+++ section7/dom-assignment-start/assignment.js
@@ -0,0 +1,14 @@
+const listItem1Sel1 = document.querySelector('#task-1');
+listItem1Sel1.style.color = 'white';
+
+const listItem1Sel2 = document.querySelector('li');
+listItem1Sel2.style.backgroundColor = 'black';
+
+const titleSel1 = document.querySelector('title');
+// titleSel1.textContent = 'Assignment - Solved!';
+
+const titleSel2 = document.head.querySelector('title');
+titleSel2.textContent = 'Assignment - Solved!'
+
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment - Solved!';
